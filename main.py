fd33e64b479dcd697d023a1b9b76829d1c5f1117
Nur Hidayah <35320146+nrhdyh@users.noreply.github.com>
2025-12-27 11:56:21 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Climate Smart Agriculture Dashboard",
+    layout="wide"
+)
+
+# Define pages
+objective1 = st.Page(
+    "home.py",
+    title="🎓 Objective 1: Education & Demographics",
+    default=True
+)
+
+objective2 = st.Page(
+    "objective2.py",
+    title="🌾 Objective 2: Land & Perception"
+)
+
+objective3 = st.Page(
+    "objective3.py",
+    title="🌱 Objective 3: Practices & Correlation"
+)
+
+# Navigation
+pg = st.navigation(
+    {
+        "Main Menu": [objective1, objective2, objective3]
+    }
+)
+
+pg.run()
