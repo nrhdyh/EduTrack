b2a3a4b999fd0c0c18723d7cc855b28c0f1803d5
Nur Hidayah <35320146+nrhdyh@users.noreply.github.com>
2025-12-27 15:49:53 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Student Dashboard", layout="wide")
 
